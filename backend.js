f50f09f063645b9dd56cd7083d8800c705a6a3dd
Abdul Mateen <125717446+Mateen-Shaikh@users.noreply.github.com>
2024-01-22 19:30:02 +0500

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,13 @@
+var input = prompt("Enter a Number for Multiplicative Table :")
+
+document.write(`${input} x 1 = `,input*1 + "<br>")
+document.write(`${input} x 2 = `,input*2 + "<br>")
+document.write(`${input} x 3 = `,input*3 + "<br>")
+document.write(`${input} x 4 = `,input*4 + "<br>")
+document.write(`${input} x 5 = `,input*5 + "<br>")
+document.write(`${input} x 6 = `,input*6 + "<br>")
+document.write(`${input} x 7 = `,input*7 + "<br>")
+document.write(`${input} x 8 = `,input*8 + "<br>")
+document.write(`${input} x 9 = `,input*9 + "<br>")
+document.write(`${input} x 10 = `,input*10 + "<br>")
+
